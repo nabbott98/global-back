79cf4cf95352a3b05dc5e30e524e65374aeb0f90
Nick Abbott <nicholasabbott98@gmail.com>
2022-10-31 11:20:10 -0400

    Update addressInfo.js
    Corrected indentation and removed trailing comma after owner object

--- app/models/addressInfo.js
+++ app/models/addressInfo.js
@@ -6,34 +6,34 @@ const addressInfoSchema = new mongoose.Schema(
 			type: String,
 			required: true,
 		},
-        phoneNumber: {
-			type: Number,
-			required: true,
-		},
-        streetAddress: {
-			type: String,
-			required: true,
-		},
-		aptAddress: String,
-        city: {
-			type: String,
-			required: true,
-		},
-        state: {
-			type: String,
-			required: true,
-		},
-        zip: {
-			type: Number,
-			required: true,
-		},
-        default: Boolean,
-        deliveryInstructions: String,
+		phoneNumber: {
+				type: Number,
+				required: true,
+			},
+		streetAddress: {
+				type: String,
+				required: true,
+			},
+			aptAddress: String,
+		city: {
+				type: String,
+				required: true,
+			},
+		state: {
+				type: String,
+				required: true,
+			},
+		zip: {
+				type: Number,
+				required: true,
+			},
+		default: Boolean,
+		deliveryInstructions: String,
 		owner: {
 			type: mongoose.Schema.Types.ObjectId,
 			ref: 'User',
 			required: true,
-		},
+		}
 	},
 	{
 		timestamps: true,
